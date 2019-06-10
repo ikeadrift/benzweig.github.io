6d01e252e7b0f969c302baf9a1694e2d936a93b4
Benjamin Zweig <benzilla@me.com>
2019-06-10 06:33:38 -0700

    add transit

---
+++ transit/precache-manifest.ae27c05eb30820b3d0e40fbf00412152.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3eb21174edbb4af4278bf161ca21a1",
+    "url": "/transit/index.html"
+  },
+  {
+    "revision": "f01d7ae76d17564ed2f3",
+    "url": "/transit/static/css/main.2535aa86.chunk.css"
+  },
+  {
+    "revision": "4a43306240a7117b71d5",
+    "url": "/transit/static/js/2.8d778672.chunk.js"
+  },
+  {
+    "revision": "f01d7ae76d17564ed2f3",
+    "url": "/transit/static/js/main.4541b41b.chunk.js"
+  },
+  {
+    "revision": "e194e18e6bd6a57c23b6",
+    "url": "/transit/static/js/runtime~main.56c7c4f4.js"
+  },
+  {
+    "revision": "adc83e0944271f35b8c423c30d57d8dd",
+    "url": "/transit/static/media/arrivaltime.adc83e09.svg"
+  },
+  {
+    "revision": "a87d7fbd749a07d501831b8c3ef3a533",
+    "url": "/transit/static/media/closeststation.a87d7fbd.svg"
+  },
+  {
+    "revision": "9f5a97effc330698b1427ce4ddc41241",
+    "url": "/transit/static/media/tripduration.9f5a97ef.svg"
+  }
+]);
